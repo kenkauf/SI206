7565bfca4113f71c570277f0bce34da960e167be
kenkauf <kenkaufman3@gmail.com>
2016-11-07 22:53:03 -0500

    commiting twitter files

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,5 +4,22 @@
 
 # You will demo this live for grading.
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+
+# print("""No output necessary although you 
+# 	can print out a success/failure message if you want to.""")
+
+import tweepy
+from twitauth import *
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+fname = 'C:/Users/kenka/Desktop/Photos/murrayintree.jpg'
+message = '#UMSI-206 #Proj3'
+# try:
+api.update_with_media(filename=fname, status=message) # API Reference: http://docs.tweepy.org/en/v3.5.0/api.html
+# except:
+#     print("Unable to download image")
--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -6,7 +6,37 @@
 # 3) Print the average polarity of the results
 
 # Be prepared to change the search term during demo.
+import tweepy
+from textblob import TextBlob
+import sys
+from twitauth import *
 
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+public_tweets = api.search('#AnnArbor')
+
+avsub = 0
+avpol = 0
+tweetcount = 0
+for tweet in public_tweets:                 # reference from https://github.com/praritlamba/Mining-Twitter-Data-for-Sentiment-Analysis
+	uprint(tweet.text)
+	analysis = TextBlob(tweet.text)
+	avpol += (analysis.sentiment.polarity)
+	avsub += (analysis.sentiment.subjectivity)
+	tweetcount += 1
+
+print("Average subjectivity is ", avsub/tweetcount)
+print("Average polarity is ", avpol/tweetcount)
